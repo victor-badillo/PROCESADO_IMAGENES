2127e487c61e59310c3349202269ea302cd8fd31
victor-badillo <victor.badillo@udc.es>
2024-11-11 14:32:26 +0100

    Corregida implementacion de edgeCanny

--- src/operaciones/bordes.py
+++ src/operaciones/bordes.py
@@ -100,22 +100,23 @@ def LoG(inImage, sigma):
 
     #Convolucionar la imagen con el kernel calculado, sombrero mexicano
     filteredImage = filterImage(inImage, kernel)
-
+    return filteredImage
     #Detectar cruces por cero
-    outImage = np.zeros_like(filteredImage)
+    # outImage = np.zeros_like(filteredImage)
 
-    threshold = 0.04
+    # threshold = 0.04
 
-    for i in range(1, filteredImage.shape[0] - 1):
-        for j in range(1, filteredImage.shape[1] - 1):
+    # for i in range(1, filteredImage.shape[0] - 1):
+    #     for j in range(1, filteredImage.shape[1] - 1):
 
-            region = filteredImage[i-1:i+2, j-1:j+2]
-            pixel = filteredImage[i, j]
-            # Detectar cruce por cero en la vecindad
-            if pixel < -threshold and np.any(region[[0, 0, 0, 1, 1, 2, 2, 2], [0, 1, 2, 0, 2, 0, 1, 2]] > threshold):  # Excluyendo el central
-                    outImage[i, j] = 1
+    #         region = filteredImage[i-1:i+2, j-1:j+2]
+    #         pixel = filteredImage[i, j]
+    #         # Detectar cruce por cero en la vecindad
+    #         if pixel < -threshold and np.any(region[[0, 0, 0, 1, 1, 2, 2, 2], [0, 1, 2, 0, 2, 0, 1, 2]] > threshold):  # Excluyendo el central
+    #                 outImage[i, j] = 1
     
-    return outImage
+    # return outImage
+    
 
 
 
@@ -132,103 +133,6 @@ outImage = edgeCanny (inImage, sigma, tlow, thigh)
     tlow, thigh: Umbrales de histéresis bajo y alto, respectivamente.
 
 '''
-'''
-def edgeCanny(inImage, sigma, tlow, thigh):
-
-    if(thigh < tlow):
-        raise ValueError("El umbral superior de histéresis debe ser superior al umbral inferior")
-    
-    #Suavizar la imagen con un filtro gaussiano
-    smoothed_image =  gaussianFilter(inImage, sigma)
-
-    visualize_image_float('filtro gaussiano', smoothed_image)
-
-
-    #Calcular los gradientes
-    gx, gy = gradientImage(smoothed_image, 'Sobel')
-    magnitude = np.sqrt(gx**2 + gy**2)             #Magnitud
-    direction = np.degrees(np.arctan2(gy, gx))     #Direccion en grados y rango [0,180]
-    direction = np.where(direction < 0, direction + 180, direction)
-
-    #AJUSTAR INTENSIDAD DE DIRECTION???
-    visualize_image_float('gradiente', magnitude)
-    #PROBAR A NORMALIZAR KERNEL EN GRADIENTES
-
-    #Supresion de no maximos
-    nms_image = np.zeros_like(magnitude, dtype=np.float64)
-    rows, cols = magnitude.shape
-
-    perp = np.zeros((rows, cols, 2), dtype=int) #Almacenamiento de las perpendiculares a la normal
-
-    #Bucle con indices adecuados para no situarse en los bordes de la imagen
-    for i in range(1, rows - 1):
-        for j in range(1, cols - 1):
-            
-            #Determinar cuales son los vecinos segun la direccion de la normal
-            if (0 <= direction[i,j] < 22.5) or (157.5 <= direction[i,j] <= 180):
-                neighbors = (magnitude[i, j + 1], magnitude[i, j - 1])  
-                perp[i,j] = (0, 1)  #Horizontal
-            elif (22.5 <= direction[i,j] < 67.5):
-                neighbors = (magnitude[i + 1, j - 1], magnitude[i - 1, j + 1])
-                perp[i,j] = (1, -1)  #Diagonal /
-            elif (67.5 <= direction[i,j] < 112.5):
-                neighbors = (magnitude[i + 1, j], magnitude[i - 1, j])
-                perp[i,j] = (1, 0)  #Vertical
-            elif (112.5 <= direction[i,j] < 157.5):
-                neighbors = (magnitude[i - 1, j - 1], magnitude[i + 1, j + 1])
-                perp[i,j] = (-1, -1)  #Diagonal \
-
-            #No es 0 si >= que el maximo de sus vecinos, que son los que estan en la direccion de la normal
-            if magnitude[i, j] >= max(neighbors):
-                nms_image[i, j] = magnitude[i, j]
-
-    visualize_image_float('supresion de no maximos',nms_image)
-    save_image_int('nms.png', nms_image)
-    #Umbralización por histeresis
-    strong_edges = (nms_image > thigh)                          #Fuertes los que son mayores que el umbral superior
-    weak_edges = ((nms_image > tlow) & (nms_image <= thigh))   #Debiles los que son menores que el umbral superior y mayores que el umbral inferior
-
-    outImage = np.zeros_like(nms_image, dtype=np.uint8)
-
-    #Marcas bordes fuertes
-    outImage[strong_edges] = 1
-
-    #Visitados
-    visited = np.zeros_like(nms_image, dtype=bool)
-
-    #Conectar bordes débiles a bordes fuertes
-    for i in range(1, rows - 1):
-        for j in range(1, cols - 1):
-            if visited[i,j]: #Si ya esta visitado continuar
-                continue
-
-            if outImage[i,j] == 1: #Si es un borde
-                
-                perp_i, perp_j = perp[i,j]    #Obtener perpendicular a la normal
-
-                
-                current_i, current_j = i+perp_i, j+perp_j #Indices de recorrido
-                #Recorrer en una direccion de la perpendicular
-                while (0 <= current_i < rows and 0 <= current_j < cols and 
-                       weak_edges[current_i, current_j] and not visited[current_i, current_j]):
-                    outImage[current_i, current_j] = 1
-                    visited[current_i, current_j] = True
-                    current_i = current_i + perp_i
-                    current_j = current_j + perp_j
-
-                current_i, current_j = i-perp_i, j-perp_j #Indices de recorrido
-                #Recorrer en la otra direccion de la perpendicular
-                while (0 <= current_i < rows and 0 <= current_j < cols and 
-                       weak_edges[current_i, current_j] and not visited[current_i, current_j]):
-                    outImage[current_i, current_j] = 1
-                    visited[current_i, current_j] = True
-                    current_i = current_i - perp_i
-                    current_j = current_j - perp_j
-
-
-    outImage = outImage.astype(inImage.dtype)
-
-    return outImage
 '''
 def edgeCanny(inImage, sigma, tlow, thigh):
 
@@ -315,5 +219,91 @@ def edgeCanny(inImage, sigma, tlow, thigh):
 
 
     outImage = outImage.astype(inImage.dtype)
+
+    return outImage
+'''
+
+def edgeCanny(inImage, sigma, tlow, thigh):
+
+    if(thigh < tlow):
+        raise ValueError("El umbral superior de histéresis debe ser superior al umbral inferior")
+    
+    #Suavizar la imagen con un filtro gaussiano
+    smoothed_image =  gaussianFilter(inImage, sigma)
+
+    visualize_image_float('suavizada_con_gauss', smoothed_image)
+
+    #Calcular los gradientes
+    gx, gy = gradientImage(smoothed_image, 'Sobel')
+    magnitude = np.sqrt(gx**2 + gy**2)             #Magnitud
+    direction = np.degrees(np.arctan2(gy, gx))     #Direccion en grados y rango [0,180]
+    direction = np.where(direction < 0, direction + 180, direction)
+
+    visualize_image_float('magnitud', magnitude)
+    visualize_image_float('direcciones', direction)
+
+    #Supresion de no maximos
+    nms_image = np.zeros_like(magnitude, dtype=np.float64)
+    rows, cols = magnitude.shape
+
+
+    #Bucle con indices adecuados para no situarse en los bordes de la imagen
+    for i in range(1, rows - 1):
+        for j in range(1, cols - 1):
+            
+            #Determinar cuales son los vecinos segun la direccion de la normal
+            if (0 <= direction[i,j] < 22.5) or (157.5 <= direction[i,j] <= 180):
+                neighbors = (magnitude[i, j + 1], magnitude[i, j - 1])  
+            elif (22.5 <= direction[i,j] < 67.5):
+                neighbors = (magnitude[i - 1, j - 1], magnitude[i + 1, j + 1])
+            elif (67.5 <= direction[i,j] < 112.5):
+                neighbors = (magnitude[i + 1, j], magnitude[i - 1, j])
+            elif (112.5 <= direction[i,j] < 157.5):
+                neighbors = (magnitude[i + 1, j - 1], magnitude[i - 1, j + 1])
+
+            #No es 0 si >= que el maximo de sus vecinos, que son los que estan en la direccion de la normal
+            if magnitude[i, j] >= max(neighbors):
+                nms_image[i, j] = magnitude[i, j]
+
+    visualize_image_float('supresion_de_no_maximos', nms_image)
+
+    #Umbralización por histeresis
+
+    #Obtener los bordes debiles
+    weak_edges = ((nms_image > tlow) & (nms_image <= thigh))
+
+    #Marcas bordes fuertes
+    outImage = (nms_image > thigh).astype(np.float64) #Float64 para seguir patron de salida
+
+    #Visitados
+    visited = np.zeros_like(nms_image, dtype=bool)
+
+    #Coordenadas para mirar alrededor de una coordenada
+    surrounding = [(-1, -1), (-1, 0), (-1, 1),(0, -1),(0, 1),(1, -1), (1, 0), (1, 1)]
+
+    while True:
+
+        prev_outImage = outImage.copy()
+
+        #Conectar bordes débiles a bordes fuertes
+        for i in range(1, rows - 1):
+            for j in range(1, cols - 1):
+                if visited[i,j]: #Si ya esta visitado continuar
+                    continue
+
+                if outImage[i,j] == 1.0: #Si es un borde fuerte
+                    visited[i,j] = True
+
+                    for offset_i,  offset_j in surrounding:
+                        ni, nj = i + offset_i, j + offset_j
+
+                        #Si la coordenada vecina es un borde debil marcar esa coordenada como borde fuerte
+                        if weak_edges[ni, nj] == 1:
+                            outImage[ni, nj] = 1.0
+
+        #Para de iterar cuando no hay mas cambios iterando
+        if np.array_equal(outImage, prev_outImage):
+            break
+        
 
     return outImage
